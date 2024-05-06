ebb3a84eb31c24ed0e6f90d314d71aee01293b4e
skt <skt@D4TECH04>
2024-05-06 16:27:29 +0200

    Added mock item data

--- OrderDB/procedures/spGetOrderItems.sql
+++ OrderDB/procedures/spGetOrderItems.sql
@@ -1,5 +1,5 @@
 ﻿CREATE PROCEDURE [dbo].[spGetOrderItems]
 	@orderId int = 0
 AS
-	SELECT * From Items WHERE ItemId in (SELECT ItemId FROM OrderItems WHERE oi.OrderId = @orderId)
+	SELECT * From Items WHERE ItemId in (SELECT ItemId FROM OrderItems WHERE OrderId = @orderId)
 RETURN 0
